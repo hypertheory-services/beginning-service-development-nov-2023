6de224661de1260056b25d1d93ccf877630583ae
Jeff Gonzalez <jeff@hypertheory.com>
2023-11-15 12:00:13 -0500

    models

---
+++ IssueTrackerSolution/IssueTrackerApi/Controllers/IssuesController.cs
@@ -0,0 +1,33 @@
+﻿using IssueTrackerApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTrackerApi.Controllers;
+
+[ApiController]
+public class IssuesController : ControllerBase
+{
+    [HttpPost("/software/{softwareId}/issues/high-priority-issues")]
+
+    public async Task<ActionResult> AddIssueAsync([FromBody] IssueCreateModel request)
+    {
+
+        // valid at this point!
+        // save it to a database,
+        // create some kind of response.
+
+        return Ok();
+    }
+
+
+    [HttpGet("/software")]
+    public async Task<ActionResult> GetSoftwareCatalog()
+    {
+        var catalog = new List<string>
+        {
+            "excel",
+            "powerpoint",
+            "vscode"
+        };
+        return Ok(catalog);
+    }
+}
